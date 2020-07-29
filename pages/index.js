e565f18d14e40c8139a8bec2ffdf9dcb4a063392
Rui Silva <rdasilvaspotzer@outlook.com>
2020-07-29 22:01:25 +0200

    adjusted spinner to show before everything else

--- pages/index.js
+++ pages/index.js
@@ -61,80 +61,85 @@ const Index = () => {
 
   return (
     <div>
-      <CustomHead />
-      <div className="hoverBoard">
-        {currentWords.length > 0 && (
-          <button
-            className="restart"
-            onClick={() => {
-              restart();
-            }}
-          >
-            Restart
-          </button>
-        )}
-        <div className="scoring">
-          <div
-            className={`score
+      {isLoading && <div className="spinner"></div>}
+      {!isLoading && (
+        <div>
+          <CustomHead />
+          <div className="hoverBoard">
+            {currentWords.length > 0 && (
+              <button
+                className="restart"
+                onClick={() => {
+                  restart();
+                }}
+              >
+                Restart
+              </button>
+            )}
+            <div className="scoring">
+              <div
+                className={`score
           ${currentWords.length > 0 && isTeam1 ? "team-active" : ""}`}
-          >
-            <strong>
-              Team 1<h2>{team1Points}</h2>
-            </strong>
-          </div>
-          <div
-            className={`score
+              >
+                <strong>
+                  Team 1<h2>{team1Points}</h2>
+                </strong>
+              </div>
+              <div
+                className={`score
           ${currentWords.length > 0 && !isTeam1 ? "team-active" : ""}`}
-          >
-            <strong>
-              Team 2<h2>{team2Points}</h2>
-            </strong>
+              >
+                <strong>
+                  Team 2<h2>{team2Points}</h2>
+                </strong>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <h1 className="title">Hey Robot!</h1>
-      <div className="final-score">
-        {currentWords.length === 0 &&
-          team1Points > team2Points &&
-          "Team 1 Wins 🙌🏻"}
-        {currentWords.length === 0 &&
-          team2Points > team1Points &&
-          "Team 2 Wins 🙌🏻"}
-        {isLoading && <div className="spinner"></div>}
-        {currentWords.length === 0 && !isLoading && (
-          <div>
-            <button
-              className="restart"
-              onClick={() => {
-                restart();
-              }}
-            >
-              Restart
-            </button>
-          </div>
-        )}
-      </div>
-      <ul>
-        {currentWords.map((w, i) => {
-          return (
-            <Card
-              key={w.word}
-              word={w.word}
-              points={w.points}
-              onGotIt={(_, points) => {
-                const remainingWords = currentWords;
-                remainingWords.splice(i, 1);
-                setCurrentWords(remainingWords);
 
-                if (isTeam1) setTeam1Points(team1Points + points);
-                else setTeam2Points(team2Points + points);
-                setIsTeam1(!isTeam1);
-              }}
-              onDamnIt={() => setIsTeam1(!isTeam1)}
-            />
-          );
-        })}
-      </ul>
+          <h1 className="title">Hey Robot!</h1>
+          <div className="final-score">
+            {currentWords.length === 0 &&
+              team1Points > team2Points &&
+              "Team 1 Wins 🙌🏻"}
+            {currentWords.length === 0 &&
+              team2Points > team1Points &&
+              "Team 2 Wins 🙌🏻"}
+            {currentWords.length === 0 && (
+              <div>
+                <button
+                  className="restart"
+                  onClick={() => {
+                    restart();
+                  }}
+                >
+                  Restart
+                </button>
+              </div>
+            )}
+          </div>
+          <ul>
+            {currentWords.map((w, i) => {
+              return (
+                <Card
+                  key={w.word}
+                  word={w.word}
+                  points={w.points}
+                  onGotIt={(_, points) => {
+                    const remainingWords = currentWords;
+                    remainingWords.splice(i, 1);
+                    setCurrentWords(remainingWords);
+
+                    if (isTeam1) setTeam1Points(team1Points + points);
+                    else setTeam2Points(team2Points + points);
+                    setIsTeam1(!isTeam1);
+                  }}
+                  onDamnIt={() => setIsTeam1(!isTeam1)}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
