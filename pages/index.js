e61b16c2bd628636a39cef8e5999cda0029675a2
Cipriano Freitas <casf1992@gmail.com>
2020-12-31 19:38:13 +0100

    temp 3 player feature

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Card from "../components/card";
-import CustomHead from "../components/CustomHead";
-import { useState, useEffect } from "react";
-const allTheWordsAvailable = require("../words.json");
+import Card from '../components/card';
+import CustomHead from '../components/CustomHead';
+import { useState, useEffect } from 'react';
+const allTheWordsAvailable = require('../words.json');
 
-const generateWords = (words) => {
+const generateWords = words => {
   const pickedWords = [];
   const wordsCloned = Object.assign([], words);
   for (let i = 0; i < 16; i++) {
@@ -25,39 +25,45 @@ const Index = () => {
   const [team1Points, setTeam1Points] = useState(0);
 
   const [team2Points, setTeam2Points] = useState(0);
+  const [team3Points, setTeam3Points] = useState(0);
 
   const [currentWords, setCurrentWords] = useState([]);
 
-  const [isTeam1, setIsTeam1] = useState(true);
+  const [isTeam, setIsTeam] = useState(1);
 
   useEffect(() => {
-    setTeam1Points(getState("team1Points", 0));
-    setTeam2Points(getState("team2Points", 0));
+    setTeam1Points(getState('team1Points', 0));
+    setTeam2Points(getState('team2Points', 0));
+    setTeam3Points(getState('team3Points', 0));
     setCurrentWords(
-      getState("currentWords", generateWords(allTheWordsAvailable))
+      getState('currentWords', generateWords(allTheWordsAvailable))
     );
-    setIsTeam1(getState("isTeam1", true));
+    setIsTeam(getState('isTeam', 1));
     setIsLoading(false);
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("team1Points", JSON.stringify(team1Points));
+    localStorage.setItem('team1Points', JSON.stringify(team1Points));
   }, [team1Points]);
   useEffect(() => {
-    localStorage.setItem("team2Points", JSON.stringify(team2Points));
+    localStorage.setItem('team2Points', JSON.stringify(team2Points));
   }, [team2Points]);
   useEffect(() => {
-    localStorage.setItem("currentWords", JSON.stringify(currentWords));
+    localStorage.setItem('team3Points', JSON.stringify(team3Points));
+  }, [team3Points]);
+  useEffect(() => {
+    localStorage.setItem('currentWords', JSON.stringify(currentWords));
   }, [currentWords, currentWords.length]);
   useEffect(() => {
-    localStorage.setItem("isTeam1", JSON.stringify(isTeam1));
-  }, [isTeam1]);
+    localStorage.setItem('isTeam', JSON.stringify(isTeam));
+  }, [isTeam]);
 
   const restart = () => {
     setTeam1Points(0);
     setTeam2Points(0);
+    setTeam3Points(0);
     setCurrentWords(generateWords(allTheWordsAvailable));
-    setIsTeam1(true);
+    setIsTeam(1);
   };
 
   return (
@@ -80,7 +86,7 @@ const Index = () => {
             <div className="scoring">
               <div
                 className={`score
-          ${currentWords.length > 0 && isTeam1 ? "team-active" : ""}`}
+          ${currentWords.length > 0 && isTeam === 1 ? 'team-active' : ''}`}
               >
                 <strong>
                   Team 1<h2>{team1Points}</h2>
@@ -88,10 +94,18 @@ const Index = () => {
               </div>
               <div
                 className={`score
-          ${currentWords.length > 0 && !isTeam1 ? "team-active" : ""}`}
+          ${currentWords.length > 0 && isTeam === 2 ? 'team-active' : ''}`}
               >
                 <strong>
                   Team 2<h2>{team2Points}</h2>
+                </strong>
+              </div>
+              <div
+                className={`score
+          ${currentWords.length > 0 && isTeam === 3 ? 'team-active' : ''}`}
+              >
+                <strong>
+                  Team 3<h2>{team3Points}</h2>
                 </strong>
               </div>
             </div>
@@ -101,10 +115,10 @@ const Index = () => {
           <div className="final-score">
             {currentWords.length === 0 &&
               team1Points > team2Points &&
-              "Team 1 Wins 🙌🏻"}
+              'Team 1 Wins 🙌🏻'}
             {currentWords.length === 0 &&
               team2Points > team1Points &&
-              "Team 2 Wins 🙌🏻"}
+              'Team 2 Wins 🙌🏻'}
             {currentWords.length === 0 && (
               <div>
                 <button
@@ -130,11 +144,18 @@ const Index = () => {
                     remainingWords.splice(i, 1);
                     setCurrentWords(remainingWords);
 
-                    if (isTeam1) setTeam1Points(team1Points + points);
-                    else setTeam2Points(team2Points + points);
-                    setIsTeam1(!isTeam1);
+                    if (isTeam === 1) setTeam1Points(team1Points + points);
+                    if (isTeam === 2) setTeam2Points(team2Points + points);
+                    if (isTeam === 3) setTeam3Points(team3Points + points);
+                    let isNewTeam = isTeam + 1;
+                    if (isNewTeam === 4) isNewTeam = 1;
+                    setIsTeam(isNewTeam);
                   }}
-                  onDamnIt={() => setIsTeam1(!isTeam1)}
+                  onDamnIt={() => {
+                    let isNewTeam = isTeam + 1;
+                    if (isNewTeam === 4) isNewTeam = 1;
+                    setIsTeam(isNewTeam);
+                  }}
                 />
               );
             })}
